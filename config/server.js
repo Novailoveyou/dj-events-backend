11b163872b2231a48df34541b3c2df830e8fe466
novailoveyou <novailoveyoux@gmail.com>
2021-04-25 17:31:48 +0300

    added ADMIN_JWT_SECRET to .env

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '43f6147cb3467c75a72ca77d57eba132'),
+      secret: env("ADMIN_JWT_SECRET", env("ADMIN_JWT_SECRET")),
     },
   },
 });
